8f3e63a2cdc4cb91056e1ea79e05c2da89783eab
francisco28 <fgallegosgo@gmail.com>
2020-06-18 11:44:12 -0600

    creating the server of Express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('From index.js');
+const express = require('express');
+
+//create the server
+const app = express();
+
+//port of the server
+const PORT = process.env.PORT || 4000;
+
+//define the main page
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+//run the server
+app.listen(PORT, () => {
+    console.log(`The server is running in the port ${PORT}`);
+});
