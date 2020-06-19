1ed5109ca54a291aeac4b524e87e6a6cb9e0b014
francisco28 <fgallegosgo@gmail.com>
2020-06-19 14:14:33 -0600

    checking if the user has not been registered

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,16 @@ const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
 
+    //extract email and password
+    const { email, password } = req.body;
+
     try {
-        let user;
+        //review that the user is unique
+        let user = await User.findOne({ email });
+
+        if(user) { 
+            return res.status(400).json({ msg: 'The user already exist' });
+        }
 
         //create the new user
         user = new User(req.body);
@@ -12,7 +20,7 @@ exports.createUser = async (req, res) => {
         await user.save();
 
         //message of confirmation
-        res.send('User created succesful');
+        res.json({ msg: 'User successfully created' });
     } catch (error) {
         console.log(error);
         res.status(400).send('There was an error');
