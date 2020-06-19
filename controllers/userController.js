3f49b9cefab7d4f50b67211986ab64b31c97fe75
francisco28 <fgallegosgo@gmail.com>
2020-06-19 12:02:02 -0600

    add the controller for create news users

---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+
+
+exports.createUser = (req, res) => {
+    console.log('From createUser');
+}
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 //routes for create users
 const express = require('express');
 const router = express.Router();
-
+const userController = require('../controllers/userController');
 
 //create an user
 // endpoint: api/users
-router.post('/', () => {
-    
-});
+router.post('/', 
+    userController.createUser
+);
 
 module.exports = router;
+
+
+//model view es quien interactua con la BD
+//controller is quien toma los request 
