a327544e3cc0bdb7bde8d54ddbfebd78df743091
francisco28 <fgallegosgo@gmail.com>
2020-06-22 16:37:44 -0600

    inserting tasks and the project to belongs

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -12,7 +12,33 @@ exports.createTask = async (req, res) => {
         return res.status(400).json({ errors: errors.array() })
     }
 
+    
+    
+    try {
 
+        //extract the project and check if exist
+        const { project } = req.body;
+    
+        
+        const existProject = await Project.findById(project);
+        if(!existProject) {
+            return res.status(404).send({ msg: 'Project not found' });
+        }
+
+        //check if the current project belongs to the user authenticated
+        if( existProject.creator.toString() !== req.user.id ) {
+            return res.status(401).send({ msg: 'Not authorized' });
+        }
+
+        //create the task
+        const task = Task(req.body);
+        await task.save();
+        res.json({ task });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ msg: 'There was a error' });
+    }
 
 
 }
