9beba696bfa1aa2cdb9eaf23e3cbe972f735b46e
francisco28 <fgallegosgo@gmail.com>
2020-06-22 10:01:33 -0600

    inserting new projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,3 +1,17 @@
+
+const Project = require('../models/Project');
+
 exports.createProject = async (req, res) => {
-    
+
+
+    try {
+        //create a new project
+        const project = new Project(req.body);
+        project.save();
+        res.json(project);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('There was an error');
+    }
 }
